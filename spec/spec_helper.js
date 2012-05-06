4e05efd1a06fd311df50328910d099174c17d9e3
David Czarnecki <czarneckid@acm.org>
2012-05-05 23:08:33 -0400

    Use DB 15 for test

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,6 +1,7 @@
 require('coffee-script');
 require('should');
 Redis = require('redis').createClient();
+Redis.select(15);
 Streak = require('../lib/');
 Streak.configure(function() {
   this.redis = Redis;
